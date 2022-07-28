0cf292dab0ade239b1d0f5d4d82cc68ccc111cc7
rbownes <rjbownes@gmail.com>
2022-07-28 10:11:14 +0100

    add first folders

---
+++ ann_usecase/tests/test_ann_usecase.py
@@ -0,0 +1,5 @@
+from ann_usecase import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
---
+++ preprocess_why_should_i_care/tests/test_preprocess_why_should_i_care.py
@@ -0,0 +1,5 @@
+from preprocess_why_should_i_care import __version__
+
+
+def test_version():
+    assert __version__ == '0.1.0'
